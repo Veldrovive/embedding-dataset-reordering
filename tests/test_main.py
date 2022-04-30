6bc732459c99072408ac05a438c3cbc1385695e9
Veldrovive <aidan.dempster@gmail.com>
2022-04-30 14:32:42 -0400

    Fixed file path of embeddings for test

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,7 +7,7 @@ def test_reorder():
     # TODO: Make this not half-assed. Or don't.
     embeddings_folder = "./examples/test_data_inference/img_emb"
     metadata_folder = "./examples/test_data_inference/metadata"
-    reordered_embeddings_folder = "./examples/test_reordered"
+    reordered_embeddings_folder = "./examples/test_reordered/reordered_embeddings"
     assert os.path.exists(embeddings_folder), f"Embeddings folder not found"
     assert os.path.exists(metadata_folder), f"Metadata folder not found"
     assert os.path.exists(reordered_embeddings_folder), f"Reordered embeddings folder not found"
